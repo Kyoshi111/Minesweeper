a1b631c753e2eafa14a9011804315abf94fb3986
Kyoshi111 <artem290803@gmail.com>
2022-11-22 19:52:59 +0300

    Added several properties and removed constructor  in Cell struct

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -2,14 +2,6 @@ using UnityEngine;
 
 public struct Cell
 {
-    public Cell(Vector2 position, Type state = Type.Empty, int minesAround = 0, bool isFlagged = false)
-    {
-        Position = position;
-        State = state;
-        MinesAround = minesAround;
-        IsFlagged = isFlagged;
-    }
-    
     public enum Type
     {
         Empty,
@@ -17,8 +9,10 @@ public struct Cell
         Number
     }
     
-    public Vector2 Position { get; private set; }
+    public Vector2 Position { get; set; }
     public Type State { get; set; }
     public int MinesAround { get; set; }
-    public bool IsFlagged { get; set; }
+    public bool Flagged { get; set; }
+    public bool Exploded { get; set; }
+    public bool Revealed { get; set; }
 }
